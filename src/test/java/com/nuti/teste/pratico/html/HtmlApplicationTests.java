0836ebd239482bf610b4f3afaa51e87950a49353
gustavokida <gustavokida@live.com>
2023-07-29 19:03:12 -0400

    first commit

---
+++ src/test/java/com/nuti/teste/pratico/html/HtmlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuti.teste.pratico.html;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
